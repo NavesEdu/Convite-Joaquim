1effe895e396db08c66a38ee1c9572a8c8455cbc
Eduardo Naves <eduardonaves41@gmail.com>
2025-09-07 22:21:44 -0300

    feat: Adiciona roteamento para múltiplas páginas de convite

--- src/components/Local.jsx
+++ src/components/Local.jsx
@@ -8,12 +8,12 @@ function Local() {
       
       <div className="local-container">
         <div className="address-info">
-          <h3>Casa da minha Bisa Maria Angélica</h3>
-          <p><strong>Endereço:</strong> Rua Eugênio de Souza, 315 - Bairro Centro</p>
-          <p><strong>Data:</strong> 30/11/2025</p>
-          <p><strong>Horário:</strong> às 16:00</p>
+          <h3>{partyData.locationName}</h3>
+          <p><strong>Endereço:</strong> {partyData.adress}</p>
+          <p><strong>Data:</strong> {partyData.date}</p>
+          <p><strong>Horário:</strong> {partyData.time}</p>
           <a 
-          href="https://www.google.com/maps/place/R.+Eug%C3%AAnio+de+Souza,+315+-+Carmo+da+Cachoeira,+MG,+37225-000/@-21.4626948,-45.2254938,17z/data=!3m1!4b1!4m6!3m5!1s0x94cab934eb85a33d:0x32449abda0c792c!8m2!3d-21.4626998!4d-45.2229189!16s%2Fg%2F11fzf30vjg?entry=ttu&g_ep=EgoyMDI1MDkwMy4wIKXMDSoASAFQAw%3D%3D" 
+          href={partyData.googleMapsLink}
           target="_blank" 
           rel="noopener noreferrer"
           className="button"
@@ -24,7 +24,7 @@ function Local() {
 
         <div className="map-container">
           <iframe 
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3713.1483557487213!2d-45.22549382394297!3d-21.462694787863164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cab934eb85a33d%3A0x32449abda0c792c!2sR.%20Eug%C3%AAnio%20de%20Souza%2C%20315%20-%20Carmo%20da%20Cachoeira%2C%20MG%2C%2037225-000!5e0!3m2!1spt-BR!2sbr!4v1757286659591!5m2!1spt-BR!2sbr"
+            src={partyData.googleMapsiframe}
             width="100%" 
             height="100%" 
             style={{ border: 0 }} 
---
+++ src/partyData.js
@@ -0,0 +1,27 @@
+// src/partyData.js
+
+// Objeto com as informações de cada festa
+export const parties = {
+  joaquim_carmo: {
+    name: "Joaquim",
+    age: "1º Aninho",
+    theme: "Dino-Festa",
+    date: "Domingo, 30 de Novembro de 2025",
+    time: "às 16:00",
+    address: "Rua Eugênio de Souza, 315 - Bairro Centro",
+    locationName: "Casa da minha Bisa Maria Angélica",
+    googleMapsLink: "https://www.google.com/maps/place/R.+Eug%C3%AAnio+de+Souza,+315+-+Carmo+da+Cachoeira,+MG,+37225-000/@-21.4626948,-45.2254938,17z/data=!3m1!4b1!4m6!3m5!1s0x94cab934eb85a33d:0x32449abda0c792c!8m2!3d-21.4626998!4d-45.2229189!16s%2Fg%2F11fzf30vjg?entry=ttu&g_ep=EgoyMDI1MDkwMy4wIKXMDSoASAFQAw%3D%3D",
+    googleMapsiframe: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3713.1483557487213!2d-45.22549382394297!3d-21.462694787863164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cab934eb85a33d%3A0x32449abda0c792c!2sR.%20Eug%C3%AAnio%20de%20Souza%2C%20315%20-%20Carmo%20da%20Cachoeira%2C%20MG%2C%2037225-000!5e0!3m2!1spt-BR!2sbr!4v1757286659591!5m2!1spt-BR!2sbr",
+  },
+  joaquim_srs: {
+    name: "Joaquim",
+    age: "1º Aninho",
+    theme: "Dino-Festa",
+    date: "Sábado, 06 de Dezembro de 2025",
+    time: "às 16:00",
+    address: "Rua José Mendes Vilela, 12 - Bairro Boa Vista",
+    locationName: "Casa da minha Vó Regina",
+    googleMapsLink: "https://www.google.com/maps/place/R.+Jos%C3%A9+Mendes+Vil%C3%A9la,+12+-+Boa+Vista+1,+Santa+Rita+do+Sapuca%C3%AD+-+MG,+37540-000/@-22.2397131,-45.7117858,17z/data=!3m1!4b1!4m6!3m5!1s0x94cba3adcbbd6ae3:0xd8b4e45c10a1e150!8m2!3d-22.2397181!4d-45.7069149!16s%2Fg%2F11w7qd47wy?entry=ttu&g_ep=EgoyMDI1MDkwMy4wIKXMDSoASAFQAw%3D%3D",
+    googleMapsiframe: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3693.0095063807107!2d-45.70691490000001!3d-22.2397181!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cba3adcbbd6ae3%3A0xd8b4e45c10a1e150!2sR.%20Jos%C3%A9%20Mendes%20Vil%C3%A9la%2C%2012%20-%20Boa%20Vista%201%2C%20Santa%20Rita%20do%20Sapuca%C3%AD%20-%20MG%2C%2037540-000!5e0!3m2!1spt-BR!2sbr!4v1757294051297!5m2!1spt-BR!2sbr"
+  }
+};
